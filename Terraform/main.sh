f44103f2ca5546738bdb748d77bb9d9540a005da
lusianneDjepang <31125427+lusianneDjepang@users.noreply.github.com>
2020-10-01 13:42:31 +0200

    Update main.sh

--- Terraform/main.sh
+++ Terraform/main.sh
@@ -1,1 +1,17 @@
+//Ubuntu/Debian
 
+//Add the HashiCorp GPG key.
+
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+//Add the official HashiCorp Linux repository.
+
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+//Update and install.
+
+sudo apt-get update && sudo apt-get install terraform
+
+//Enable Tab Completion
+
+terraform -install-autocomplete
